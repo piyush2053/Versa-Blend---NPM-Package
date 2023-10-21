df507c1a19d6ccf43ecc30cc88dcfb82b5d50aa7
piyush.patel <piyush.patel@rws.com>
2023-10-21 13:51:23 +0530

    Export fix

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-function sum(a,b){
-    return a+b
-}
-function multiply(a,b){
-    return a*b
-}
-console.log("Hii")
+function sum(a, b) {
+    return a + b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  console.log("Hii");
+  
+  module.exports = { sum, multiply };
+  
